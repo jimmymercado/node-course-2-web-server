9c854e5df969c8adc16dfb214531b5d2b70e04c6
Jimmy Mercado <jimmy.mercado@live.com>
2017-08-01 16:30:53 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
 
 app.use((req, res, next) => {
@@ -23,6 +23,10 @@ app.use((req, res, next) => {
     next();
 });
 
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+
+// });
 
 
 app.use(express.static(__dirname + '/public'));
